1f059c6ba8e8505145dbbf74bb274c7c520bbfd8
cakraLangit <fatirajiahmad@gmail.com>
2025-05-13 13:57:09 +0700

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+// app.module.ts
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // agar .env bisa diakses dari mana pun
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(  "5432"),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // set false kalau sudah di production
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
